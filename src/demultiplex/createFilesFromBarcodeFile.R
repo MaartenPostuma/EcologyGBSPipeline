f490145d00429b3f909af4f1b8ad3adf297ed8ed
MaartenPostuma <smart-post@hotmail.com>
2024-01-31 10:14:48 +0100

    Update createFilesFromBarcodeFile.R

--- src/demultiplex/createFilesFromBarcodeFile.R
+++ src/demultiplex/createFilesFromBarcodeFile.R
@@ -9,7 +9,7 @@ barcodes$run<-sub("_R1.fq.gz","",barcodes$rawR1)
 barcodeStacks<-data.frame(sample=barcodes$sample,
                           barcode1=paste0(barcodes$barcode1,"C"),
                           barcode2=paste0(barcodes$barcode2,"C"))
-write.table(barcodeStacks,paste0(args[2]"/barcodeStacks.tsv"),row.names = F,col.names = F,quote=F,sep="\t")
+write.table(barcodeStacks,paste0(args[2],"/barcodeStacks.tsv"),row.names = F,col.names = F,quote=F,sep="\t")
 
 for(run in barcodes$run){
   barcodesSub<-barcodes[barcodes$run==run,]
