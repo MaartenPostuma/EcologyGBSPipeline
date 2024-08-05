bec93fe580d3eb64fc6ff17a42fe53e1dc03d013
MaartenPostuma <smart-post@hotmail.com>
2024-08-05 15:21:55 +0200

    stopped clustering from erroring

--- src/filterAndFigures/cluster.R
+++ src/filterAndFigures/cluster.R
@@ -1,12 +1,12 @@
 library(SNPRelate)
 library(ggdendro)
 args = commandArgs(trailingOnly=TRUE)
-#dataPop<-read.table("results/stacksFiles/SNPFilterPopMap.tsv",h=T)
+#dataPop<-read.table("resultsBosbingelkruid/stacksFiles/SNPFilterPopMap.tsv",h=T)
 dataPop<-read.table(args[1],h=T)
-#gdsFile<-"results/filters/max_missing~0.1/maf~0.01/populations.snps.gds"
+#gdsFile<-"resultsBosbingelkruid/filters/max_missing~0.8/maf~0.025/populations.snps.gds"
 gdsFile<-args[2]
 popmap<-read.table(args[3])
-#popmap<-read.table("results/stacksFiles/popmapFiltered.tsv")
+#popmap<-read.table("resultsBosbingelkruid/stacksFiles/popmapFiltered.tsv")
 genofile<-snpgdsOpen(gdsFile)
 
 colnames(popmap)<-c("sample.id","pop")
@@ -23,7 +23,12 @@ diss<-snpgdsDiss(genofile,autosome.only=F)
 #diss$diss<-diss$diss[-checkForBrokenInds2,-checkForBrokenInds2]
 #diss$sample.id<-diss$sample[-checkForBrokenInds2]
 ########################################
+splittedPath<-strsplit(gdsFile,split="/")[[1]]
+max_missingString<-splittedPath[grep("max_missing",splittedPath)]
+mafString<-splittedPath[grep("maf~",splittedPath)]
 
+
+tryCatch({
 hc<-snpgdsHCluster(diss)
 
 fit <- hclust(as.dist(hc$dist), method="ward.D") 
@@ -33,16 +38,19 @@ treeLabels<-dend_data$labels
 treeLabelsFinal<-merge(treeLabels,popmap,by.x="label",by.y="sample.id",sort=F)
 
 
-splittedPath<-strsplit(gdsFile,split="/")[[1]]
-max_missingString<-splittedPath[grep("max_missing",splittedPath)]
-treeLabelsFinal$max_missing<-sub("^.*~","",max_missingString)
-treeSegments$max_missing<-sub("^.*~","",max_missingString)
-mafString<-splittedPath[grep("maf~",splittedPath)]
 treeLabelsFinal$maf<-sub("^.*~","",mafString)
+treeLabelsFinal$max_missing<-sub("^.*~","",max_missingString)
 treeSegments$maf<-sub("^.*~","",mafString)
+treeSegments$max_missing<-sub("^.*~","",max_missingString)
 treeLabelsFinal$nSNPs<-nSNPs
 treeLabelsFinal<-merge(treeLabelsFinal,dataPop,by="pop")
 write.table(treeLabelsFinal,args[4],row.names=F,quote=F)
 write.table(treeSegments,args[5],row.names=F,quote=F)
 
 snpgdsClose(genofile)
+},error=function(cond) {
+            message(conditionMessage(cond))
+            write.table(data.frame(pop=NA,label=NA,x=NA,y=NA,max_missing=max_missingString,maf=mafString, nSNPs=0, metaPop=NA),args[4],row.names=F,quote=F)
+            write.table(data.frame(x=NA,y=NA,xend=NA,yend=NA,max_missing=max_missingString,maf=mafString),args[5],row.names=F,quote=F)
+
+})
